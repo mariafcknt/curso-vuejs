2399bc2b8c483d846203ebee71d2b7023bd0a7a0
Maria <mariafcknt@outlook.com>
2024-05-03 12:01:52 -0300

    aula 3

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>Mudei kk</p>
+    <h1>❤️</h1>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .header {
+        background: #000;
+        color: white;
+    }
+</style>
