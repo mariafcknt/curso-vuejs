a8eb9fdaf669feab08e48b850dbef0da374f5f40
Maria <mariafcknt@outlook.com>
2024-05-09 11:11:17 -0300

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,85 +1,34 @@
 <template>
-
   <div>
 
-    <div>
-      One-way data binding <br>
-      Two-way data binding <br>
-      v-model -> formulários
+    <button @click.once="onClick">
+      Enviar
+    </button>
+
+    <br><br>
+
+    <div @mouseover="onMouseOver"
+        @mouseout="onMouseOut()" 
+    >
+      Mouse over
     </div>
 
     <br><br>
 
-    <div>
-      <label>Nome</label><br>
-      <input
-        v-model="name"
+    <form
+      action="https://google.com"
+      v-on:submit.prevent="onSubmit"
+    >
+      <input 
         type="text"
-      > <br>
-      {{ name }}
-
-      <br><br>
-
-      <div>
-        <label>Sports</label><br>
-        <select v-model="sports">
-          <option value="">Escolha</option>
-          <option value="futebol">Futebol</option>
-          <option value="skate">Skate</option>
-          <option value="tenis">Tenis</option>
-        </select> <br>
-        {{ sports }}
-      </div>
-
-      <br><br>
-
-      <div>
-        <label>Newsletter</label> <br>
-        <input
-          v-model="newsletter"
-          type="radio"
-          value="Sim"
-        > Sim
-        <input
-          v-model="newsletter"
-          type="radio"
-          value="Não"
-        > Não <br>
-        {{ newsletter }}
-      </div>
-
-      <br><br>
-
-      <div>
-        <label>Contrato</label> <br>
-        <input
-          v-model="contract"
-          type="checkbox"
-        > Aceita nossos termos... <br>
-        {{ contract }}
-      </div>
-
-      <br><br>
-
-      <div>
-        <label>Cores que você mais gosta</label> <br>
-        <input
-          v-model="colors"
-          type="checkbox"
-          value="Azul"
-        > Azul 
-
-        <input
-          v-model="colors"
-          type="checkbox"
-          value="Amarelo"
-        > Amarelo <br>
-        {{ colors }}
-      </div>
-
-    </div>
+        @keyup.enter="onKeyUp"
+      >
+      <button type=submit>
+        Enviar
+      </button>
+    </form>
+    
   </div>
-
 </template>
 
 <script>
@@ -91,41 +40,31 @@ export default {
   },
   data() {
     return {
-      name: '',
-      sports: 'futebol',
-      newsletter: '',
-      contract: 'true',
-      colors: [] 
+      
+    }
+  },
+
+  methods: {
+    onClick($evt) {
+      console.log('click', $evt)
+    },
+    onMouseOver($evt) {
+      console.log('mouse over', $evt)
+    },
+    onMouseOut() {
+      console.log('mouse out')
+    },
+    onSubmit() {
+      console.log('submit')
+    },
+    onKeyUp($evt) {
+      console.log('onKeyUp', $evt)
     }
   }
 }
 </script>
 
 <style>
-.title {
-  font-size: 20px;
-  color: blue;
-}
-
-.title-home {
-  font-size : 40px;
-  color: green;
-}
-
-.text {
-  color: #14a094;
-}
-
-.text-home {
-  color: purple;
-}
-
-.todos-item {
-  background: #000;
-  margin: 0 0 5px 0;
-  padding: 3px 6px;
-  color: #ffff;
-}
 
 #app {
   font-family: Avenir, Arial, Helvetica, sans-serif;
@@ -134,4 +73,5 @@ export default {
   color: #2c3e50;
   margin: 60px;
 }
+
 </style>
