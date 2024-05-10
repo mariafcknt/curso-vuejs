87d8f845df49ea71f5d6a54b9e5a88e930c560ed
Maria <mariafcknt@outlook.com>
2024-05-10 15:10:50 -0300

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,4 +1,33 @@
 <template>
+
+  <div>
+    <input
+      v-model="name"
+      type="text"
+    > <br>
+    {{ name }}
+  </div>
+
+  <br><br><br>
+
+  <input
+      v-model="user.first_name"
+      type="text"
+    > <br>
+  <br>
+    <input
+      v-model="user.last_name"
+      type="text"
+    > <br>
+    {{ user.first_name }} {{ user.last_name }} 
+
+  <br><br><br>
+  <select v-model="pageCount">
+    <option value="5">5</option>
+    <option value="10">10</option>
+    <option value="15">15</option>
+  </select> <br>
+  {{ pageCount }}
  
 </template>
 
@@ -11,7 +40,43 @@ export default {
   },
   data() {
     return {
-      
+      name: '',
+      pageCount: 5,
+      user: {
+        first_name: '',
+        last_name: ''
+      }
+    }
+  },
+
+  watch: {
+    name(newValue) {
+      if (newValue.length >= 3) {
+        this.saveUserName()
+      }
+    },
+    pageCount() {
+      this.changePage();
+    },
+    user: {
+      handler() {
+        console.log('User alterado')
+      },
+      deep: true
+    }
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    saveUserName() {
+      console.log('Ajax')
+      console.log(this.name);
+    },
+    changePage() {
+      console.log('Ajax changePage')
     }
   }
 }
