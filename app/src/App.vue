e1c3d07ba38397e6b27d7fc97e8d391ce67b34fe
Maria <mariafcknt@outlook.com>
2024-05-10 14:44:55 -0300

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,5 @@
 <template>
-
-  <div>
-
-    <h1 :class="{ 'title': true, 'title-home': isHome }">Curso Vue 3</h1>
-
-    <p :class="pClass">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ducimus, harum magni blanditiis amet et iste facilis atque culpa voluptatem itaque quis magnam numquam nobis ipsum quia molestias doloremque temporibus?
-    </p>
-
-    <p :style="styleClass">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore totam iusto ipsa, asperiores corporis error, sunt neque, cupiditate possimus magnam hic voluptates atque! Sed quae explicabo nesciunt ipsam non est?
-    </p>
-
-    <div
-      v-for="(obj, index) in todos"
-      :key="obj.id"
-      class="todos-item"
-    > 
-      <img
-      v-if="obj.imgSrc"
-      v-bind:src="obj.imgSrc">
-      {{ index + 1}} - {{ obj.title }}
-    </div>
-
-  </div>
-
+ 
 </template>
 
 <script>
@@ -36,74 +11,13 @@ export default {
   },
   data() {
     return {
-      showHeader: true,
-      classVar: 'title',
-      styleClass: {color: 'cadetblue', backgroundColor: 'black', 'font-size': '20px'},
-      pClass: ['text', 'text-home'],
-      todos: [
-      {
-          "userId": 1,
-          "id": 1,
-          "title": "delectus aut autem",
-          "completed": false,
-          "imgSrc": "https://via.placeholder.com/150"
-      },
-      {
-          "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "completed": false,
-          "imgSrc": "https://via.placeholder.com/150"
-      },
-      {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-      },
-      {
-          "userId": 1,
-          "id": 4,
-          "title": "et porro tempora",
-          "completed": true
-      },
-      {
-          "userId": 1,
-          "id": 5,
-          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-          "completed": false
-        }]
+      
     }
   }
 }
 </script>
 
 <style>
-.title {
-  font-size: 20px;
-  color: blue;
-}
-
-.title-home {
-  font-size : 40px;
-  color: green;
-}
-
-.text {
-  color: #14a094;
-}
-
-.text-home {
-  color: purple;
-}
-
-.todos-item {
-  background: #000;
-  margin: 0 0 5px 0;
-  padding: 3px 6px;
-  color: #ffff;
-}
-
 #app {
   font-family: Avenir, Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
